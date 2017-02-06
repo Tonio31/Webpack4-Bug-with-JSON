f9353d41bd37bced51a685840525326e061730a3
Antoine Pissot <antoine.pissot@potentialife.com>
2017-02-06 17:46:01 +0000

    userDataFactory file:wq

---
+++ client/app/common/userDataFactory/userData.factory.js
@@ -0,0 +1,33 @@
+let UserData = function ($resource) {
+  "ngInject";
+
+  let user = {
+    userid: "12",
+    securityToken: "dbhk&*&(dsh^£{}FDSgy26vU*(£^$Qbfb",
+    firstName: "Matthew",
+    lastName: ""
+  };
+
+
+  let getFirstName = () => {
+    return user.firstName;
+  };
+
+
+  let getSecurityToken = () => {
+    return user.securityToken;
+  };
+
+  let getUserid = () => {
+    return user.userid;
+  };
+
+  let setUserid = (userid) => {
+    user.userid = userid;
+  };
+
+  return { getFirstName, getUserid, setUserid, getSecurityToken };
+
+};
+
+export default UserData;
