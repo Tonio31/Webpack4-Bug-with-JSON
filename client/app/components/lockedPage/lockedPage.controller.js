82a7ac9bd971d08ed92074b5967ee1ac5324a7a7
Antoine Pissot <antoine.pissot@potentialife.com>
2017-03-16 12:03:05 +0000

    Fix bug on fail login

--- client/app/components/lockedPage/lockedPage.controller.js
+++ client/app/components/lockedPage/lockedPage.controller.js
@@ -1,9 +1,11 @@
 class LockedPageController {
-  constructor($log) {
+  constructor($log, ICON_FONTELLO) {
     'ngInject';
 
     // eslint-disable-next-line no-param-reassign
     $log = $log.getInstance( 'LockedPageController' );
+
+    this.ICON_FONTELLO = ICON_FONTELLO;
 
     $log.log('Nothing to initialize or do for the lockedPage');
   }
--- client/app/components/lockedPage/lockedPage.js
+++ client/app/components/lockedPage/lockedPage.js
@@ -2,9 +2,11 @@ import angular from 'angular';
 import uiRouter from 'angular-ui-router';
 import LogDecorator from 'common/logDecorator/logDecorator';
 import lockedPageComponent from './lockedPage.component';
+import ConstantModule from 'common/constants';
 
 let lockedPageModule = angular.module('lockedPage', [
   uiRouter,
+  ConstantModule,
   LogDecorator
 ])
   .component('lockedPage', lockedPageComponent)
