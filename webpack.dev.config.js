d8534a76b2c63f22c53e7944d987d42f8d6a7a32
Mark Chambers <me@marksy.com>
2017-03-17 13:43:23 +0000

    fixed merge issue, these files were probably skipped/ignored accidetally - sorry

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -5,38 +5,41 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CssSourcemapPlugin = require('css-sourcemaps-webpack-plugin');
 const WebpackNotifierPlugin = require('webpack-notifier');
 
-config.output = {
-  filename: '[name].bundle.js',
-  publicPath: '/',
-  path: path.resolve(__dirname, 'client')
+module.exports = () => {
+
+  config.output = {
+    filename: '[name].bundle.js',
+    publicPath: '/',
+    path: path.resolve(__dirname, 'client')
+  };
+
+  config.plugins = config.plugins.concat([
+
+    // Injects bundles in your index.html instead of wiring all manually.
+    // It also adds hash to all injected assets so we don't have problems
+    // with cache purging during deployment.
+    new HtmlWebpackPlugin({
+      template: 'client/indexMockBackEnd.html',
+      //template: 'client/index.html',
+      inject: 'body',
+      hash: true
+    }),
+
+    new webpack.DefinePlugin({
+      ENVIRONMENT: JSON.stringify('development')
+    }),
+
+    // Adds webpack HMR support. It act's like livereload,
+    // reloading page after webpack rebuilt modules.
+    // It also updates stylesheets and inline assets without page reloading.
+    new webpack.HotModuleReplacementPlugin(),
+
+    new CssSourcemapPlugin(),
+
+    // displays desktop notifications on MacOS
+    new WebpackNotifierPlugin()
+
+  ]);
+
+  return config;
 };
-
-config.plugins = config.plugins.concat([
-
-  // Injects bundles in your index.html instead of wiring all manually.
-  // It also adds hash to all injected assets so we don't have problems
-  // with cache purging during deployment.
-  new HtmlWebpackPlugin({
-    template: 'client/indexMockBackEnd.html',
-    //template: 'client/index.html',
-    inject: 'body',
-    hash: true
-  }),
-
-  new webpack.DefinePlugin({
-    ENVIRONMENT: JSON.stringify('development')
-  }),
-
-  // Adds webpack HMR support. It act's like livereload,
-  // reloading page after webpack rebuilt modules.
-  // It also updates stylesheets and inline assets without page reloading.
-  new webpack.HotModuleReplacementPlugin(),
-
-  new CssSourcemapPlugin(),
-
-  // displays desktop notifications on MacOS
-  new WebpackNotifierPlugin()
-
-]);
-
-module.exports = config;
