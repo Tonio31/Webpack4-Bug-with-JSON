1f0f05f6ac98d87bd13eb57359efb676f9b2f614
Mark Chambers <me@marksy.com>
2017-04-03 14:04:45 +0100

    tidied up commit

--- client/app/common/navbar/navbar.directive.js
+++ client/app/common/navbar/navbar.directive.js
@@ -13,9 +13,9 @@ let syncMenuAndState = function($rootScope, $log) {
       let showMenuPanel = (el) => {
         angular.element(el.getElementsByClassName('left-submenu')[0]).addClass('move-right');
         angular.element(el.getElementsByClassName('right-submenu')[0]).addClass('move-left');
+
         angular.element(el.parentElement).addClass('show-this-nav');
         angular.element(el.parentElement.parentElement).addClass('fix-nav-under');
-
       };
 
       let hideMenuPanel = (el, removeFixNavBar) => {
@@ -70,22 +70,13 @@ let syncMenuAndState = function($rootScope, $log) {
         event.stopPropagation();
         showMenuPanel(element[0]);
         $log.log('Menu Item Clicked!! element=', element[0].id, '   event=', event);
-        // $log.info('[menu item] TOP OF THE MENU, send `false` to logout button');
       });
 
       // If we click on Back button, we need to hide the menu
       angular.element(element[0].getElementsByClassName('back')[0]).on('click', (event) => {
-        // let grandParent = element[0].parentElement.parentElement.parentElement;
         event.stopPropagation();
         hideMenuPanel(element[0], true);
         $log.log('BACK Clicked!! element=', element[0].id, '   event=', event);
-
-        // if (grandParent.classList.contains('off-canvas-list-bugfix')) {
-        //   $log.info('TOP OF THE MENU, send `true` to logout button');
-        // }
-        // else {
-        //   $log.info('[back] TOP OF THE MENU, send `false` to logout button');
-        // }
       });
     }
   };
