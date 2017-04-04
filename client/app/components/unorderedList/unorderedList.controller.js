c554ac0eb2246843cab5f63a9795654be8efc42e
Antoine Pissot <antoine.pissot@potentialife.com>
2017-04-04 11:24:30 +0100

    Show More/Less button on unordered list

--- client/app/components/unorderedList/unorderedList.controller.js
+++ client/app/components/unorderedList/unorderedList.controller.js
@@ -5,10 +5,27 @@ class UnorderedListController {
     // eslint-disable-next-line no-param-reassign
     $log = $log.getInstance( 'UnorderedListController' );
 
+    this.MIN_ELEMENTS_DISPLAYED = 5;
+    this.showMoreButtonDisplayed = true;
+    this.limit = this.MIN_ELEMENTS_DISPLAYED;
+
     this.$onInit = () => {
       this.blockData = this.block.data;
       $log.info('this.blockData', this.blockData);
     };
+
+    this.toggleMore = () => {
+
+      this.showMoreButtonDisplayed = !this.showMoreButtonDisplayed;
+
+      if ( this.showMoreButtonDisplayed ) {
+        this.limit = this.MIN_ELEMENTS_DISPLAYED;
+      }
+      else {
+        this.limit = undefined; // see limitTo docs: "If limit is undefined, the input will be returned unchanged."
+      }
+    };
+
 
   }
 }
