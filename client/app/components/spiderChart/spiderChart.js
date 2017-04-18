a5c585705b925f2fdfe785dc1e2bb702be30a18c
Mark Chambers <me@marksy.com>
2017-04-18 11:20:56 +0100

    commented out the spider chart thing

--- client/app/components/spiderChart/spiderChart.js
+++ client/app/components/spiderChart/spiderChart.js
@@ -3,11 +3,11 @@ import uiRouter from 'angular-ui-router';
 import LogDecorator from 'common/logDecorator/logDecorator';
 import spiderChartComponent from './spiderChart.component';
 
-import RadarChart from '../../lib/radar-chart/radar-chart';
+// import RadarChart from '../../lib/radar-chart/radar-chart';
 
 let spiderChartModule = angular.module('spiderChart', [
   uiRouter,
-  //RadarChart,
+  // RadarChart,
   LogDecorator
 ])
 
