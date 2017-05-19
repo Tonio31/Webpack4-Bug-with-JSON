133f70939e500a5926a5d48e22c31646ba19707c
Antoine Pissot <antoine.pissot@potentialife.com>
2017-05-19 11:09:22 +0100

    remove comment in protractor.conf

--- protractor.conf.js
+++ protractor.conf.js
@@ -4,37 +4,25 @@ let SpecReporter = require('jasmine-spec-reporter').SpecReporter;
 var path = require('path');
 
 exports.config = {
-  specs: [
-   // './e2eTesting/Cycle1_module1.spec.js',
-    './e2eTesting/Cycle1_module2.spec.js'
-   // './e2eTesting/*.spec.js'
-  ],
 
   // seleniumAddress: 'http://localhost:4444/wd/hub',
   seleniumServerJar: './node_modules/protractor/node_modules/webdriver-manager/selenium/selenium-server-standalone-3.4.0.jar',
 
   capabilities: {
     browserName: 'chrome'
-    //'shardTestFiles': true,
-
-    // chromeOptions: {
-    //   args: [ "--headless" ]
-    // }
-    // 'maxInstances': 3
   },
 
   // Maximum time Protractor will wait for Angularjs to finish all asynch requests (in ms)
   allScriptsTimeout: 30000,
 
   // url where your app is running, relative URLs are prepending with this URL
+  // This will be overridden by gulp
   baseUrl: '',
-  // chromeOnly:true,
-  // directConnect: true,
 
   jasmineNodeOpts: {
     showColors: true,
     defaultTimeoutInterval: 30000,
-    print: () => {}
+    print: () => {} // Replace the default output by jasmine-spec-reporter.
   },
 
   // testing framework, jasmine is the default
