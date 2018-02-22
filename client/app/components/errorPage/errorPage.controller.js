95f6f2cdac605f7111ed039f7cd74d9e19ddaaa4
Antoine Pissot <antoine.pissot@potentialife.com>
2018-02-22 17:25:22 +0000

    dont send an error to bugsnag if the user goes directly on the error page

--- client/app/components/errorPage/errorPage.controller.js
+++ client/app/components/errorPage/errorPage.controller.js
@@ -25,22 +25,23 @@ class ErrorPageController {
     };
 
     this.$onInit = () => {
+
       this.errorMsg = '';
       let errorCodeToTranslate = $stateParams.errorMsg;
       if ( errorCodeToTranslate ) {
         this.errorMsg = $filter('translate')(errorCodeToTranslate).toString();
+
+        this.displayContactUsForm = $stateParams.displayMenu && JwtFactory.isLoginInfoAvailable();
+
+        let customData = Object.assign({}, $stateParams.bugsnagMetaData);
+        let errorName = $stateParams.bugsnagErrorName || 'User on Error Page';
+
+        this.sendErrorToBugsnag(customData, errorName);
       }
       else {
         this.errorMsg = $filter('translate')('ERROR_UNEXPECTED').toString();
       }
 
-
-      this.displayContactUsForm = $stateParams.displayMenu && JwtFactory.isLoginInfoAvailable();
-
-      let customData = Object.assign({}, $stateParams.bugsnagMetaData);
-      let errorName = $stateParams.bugsnagErrorName || 'User on Error Page';
-
-      this.sendErrorToBugsnag(customData, errorName);
     };
 
     this.openWendeskWidget = () => {
